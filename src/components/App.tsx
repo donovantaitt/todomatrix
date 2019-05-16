bc13a26affc6d54da02e11726a62e408f0a76f7e
Donovan Taitt <donovan.w.taitt@gmail.com>
2019-05-15 22:45:55 -0400

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
